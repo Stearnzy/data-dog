d9428e6bea457be220f97c69386e14a412d8c090
Zach Stearns <zach.stearns92@gmail.com>
2020-12-03 10:03:15 -0700

    Write test, add stub_request and include json response as read file

--- spec/features/user_can_search_for_dogs_spec.rb
+++ spec/features/user_can_search_for_dogs_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 describe "User can search for dogs" do
   it "and can see results on the page" do
+
+    json_response = File.read("spec/fixtures/dog_search.json")
+
+    stub_request(:get, "https://api.thedogapi.com/v1/images/search?limit=5").
+         with(
+           headers: {
+       	  'X-Api-Key'=>'1f9ba27b-fc22-47a4-a870-c030c16f4f78'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
     visit root_path
     fill_in :number,	with: "5"
     click_button "Fetch Dogs"
@@ -9,24 +19,11 @@ describe "User can search for dogs" do
     expect(current_path).to eq(search_path)
     expect(page).to have_content('5 Results')
 
-    within '.dog' do
+    within(first('.dog')) do
       expect(page).to have_css('.image')
       expect(page).to have_css('.breed')
       expect(page).to have_css('.life-span')
       expect(page).to have_css('.temperament')
     end
   end
-  
 end
-
-# As a user,
-# When I visit the "/"
-# And I enter "5" into the number field
-# And I click on "Fetch Dogs"
-# Then my path should be "/search"
-# And I should see a message "5 Results"
-# And I should see the following information for each result:
-#   - image
-#   - breed name
-#   - life span
-#   - temperament
