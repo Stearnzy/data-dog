fa4c9aa1dbbf0ae34ae1dbae1467cbe29ead5d86
Zach Stearns <zach.stearns92@gmail.com>
2020-12-03 10:53:41 -0700

    comment out stub for webmock and insert vcr block

--- spec/features/user_can_search_for_dogs_spec.rb
+++ spec/features/user_can_search_for_dogs_spec.rb
@@ -3,27 +3,29 @@ require 'rails_helper'
 describe "User can search for dogs" do
   it "and can see results on the page" do
 
-    json_response = File.read("spec/fixtures/dog_search.json")
+    # json_response = File.read("spec/fixtures/dog_search.json")
 
-    stub_request(:get, "https://api.thedogapi.com/v1/images/search?limit=5").
-         with(
-           headers: {
-       	  'X-Api-Key'=>'1f9ba27b-fc22-47a4-a870-c030c16f4f78'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
+    # stub_request(:get, "https://api.thedogapi.com/v1/images/search?limit=5").
+    #      with(
+    #        headers: {
+    #    	  'X-Api-Key'=>'INSERT API KEY HERE'
+    #        }).
+    #      to_return(status: 200, body: json_response, headers: {})
 
-    visit root_path
-    fill_in :number,	with: "5"
-    click_button "Fetch Dogs"
-
-    expect(current_path).to eq(search_path)
-    expect(page).to have_content('5 Results')
-
-    within(first('.dog')) do
-      expect(page).to have_css('.image')
-      expect(page).to have_css('.breed')
-      expect(page).to have_css('.life-span')
-      expect(page).to have_css('.temperament')
+    VCR.use_cassette('dog_search') do
+      visit root_path
+      fill_in :number,	with: "5"
+      click_button "Fetch Dogs"
+  
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content('5 Results')
+  
+      within(first('.dog')) do
+        expect(page).to have_css('.image')
+        expect(page).to have_css('.breed')
+        expect(page).to have_css('.life-span')
+        expect(page).to have_css('.temperament')
+      end
     end
   end
 end
